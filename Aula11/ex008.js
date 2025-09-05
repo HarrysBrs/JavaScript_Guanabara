960b62b77efe63ac971e7102acce3b6eaafa6621
HarrysBrs <hennry-luis@outlook.com>
2025-09-05 16:01:22 +0000

    Estudo dia 05/09/2025

---
+++ Aula11/ex008.js
@@ -0,0 +1,10 @@
+console.log('Olá, Mundo!')
+
+var vel = 78.2
+
+console.log(`A velocidade do seu Carro  é de ${vel}KM/h`)
+
+if (vel > 60 ) {
+    console.log ("Você ultrapassou o limite de Velocidade. MULTADO")
+} 
+console.log("Dirija sempre com cinto de segurança!")
